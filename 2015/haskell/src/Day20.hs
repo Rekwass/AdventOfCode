963ed2d3fb2c82104b601b4f373c8b2f119487ef
Rekwass <rekwasss@gmail.com>
2025-09-16 22:04:13 +0200

    feat: day20

--- 2015/haskell/src/Day20.hs
+++ 2015/haskell/src/Day20.hs
@@ -1,10 +1,10 @@
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Day20 (day20a, day20b) where
 
-{------------------------------{ 1st part }------------------------------}
+import Data.List (group, find)
+import Data.Maybe (fromJust)
 
-type House = Int
-type Elf = Int
+{------------------------------{ 1st part }------------------------------}
 
 day20a :: String -> String
 day20a = show . part1 . parseLines . lines
@@ -12,26 +12,38 @@ day20a = show . part1 . parseLines . lines
 parseLines :: [String] -> Int
 parseLines = read . head
 
-houses :: [House]
-houses = [1..]
+part1 :: Int -> Int
+part1 n = fromJust $ find ((\d -> d >= n `div` 10) . goodElfs) [1..]
 
--- elves :: [Elf]
--- elves = [[x, x * 2..] | x <- [1..]]
+goodElfs :: Int -> Int
+goodElfs = sum . divisors
 
-elvesNb :: [Elf]
-elvesNb = [1..]
+divisors :: Int -> [Int]
+divisors = map product . mapM (scanl (*) 1) . group . factorize
 
--- part1 :: Int -> Int
-part1 p = ((+1) . length) $ takeWhile (< p) $ countHousePresents houses elvesNb
--- part1 p = countHousePresents houses elvesNb !! 100000
+primes :: [Int]
+primes = 2 : filter isPrime [3,5..]
+  where
+    isPrime n = all (\p -> n `mod` p /= 0) $ takeWhile (\d -> d*d <= n) primes
 
-countHousePresents :: [House] -> [Elf] -> [Int]
-countHousePresents (h:hs) es = nbPresent : countHousePresents hs es
-    where
-      elves = filter (\x -> h `mod` x == 0) (take h es)
-      nbPresent = sum $ map (*10) elves
+factorize :: Int -> [Int]
+factorize 1 = []
+factorize n = go n primes
+  where
+    go m (p:ps)
+      | p * p > m = [m | m > 1]
+      | r == 0    = p : go q (p:ps)
+      | otherwise = go m ps
+      where
+        (q, r) = m `divMod` p
 
 {------------------------------{ 2nd part }------------------------------}
 
 day20b :: String -> String
-day20b = show . parseLines . lines
+day20b = show . part2 . parseLines . lines
+
+part2 :: Int -> Int
+part2 n = fromJust $ find ((\d -> d >= n `div` 11) . lazyElfs) [1..]
+
+lazyElfs :: Int -> Int
+lazyElfs n = sum . filter (\d -> d * 50 >= n) . divisors $ n
