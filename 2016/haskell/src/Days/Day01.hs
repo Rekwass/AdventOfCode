0ede07c16a22c94fafed0ac085884b6dd0a0f123
Rekwass <rekwasss@gmail.com>
2025-10-28 09:46:55 +0100

    update: day01 remove CoordD type

--- 2016/haskell/src/Days/Day01.hs
+++ 2016/haskell/src/Days/Day01.hs
@@ -13,7 +13,6 @@ import Lib.Numbers (manhattan, range)
 data Turn = L Int | R Int deriving Show
 data Direction = N | E | S | W deriving (Show, Eq, Ord)
 
-type CoordD = (Int, Int, Direction)
 type Coord = (Int, Int)
 
 day01a :: String -> String
@@ -26,10 +25,10 @@ parseLine = map toTurn . splitOn ", "
     toTurn ('L':xs) = L  $ read xs
 
 part1 :: [Turn] -> Int
-part1 = manhattan . toCoord . foldl' step (0, 0, N)
+part1 = manhattan . fst . foldl' step ((0, 0), N)
 
-step :: CoordD -> Turn -> CoordD
-step (y, x, d) t = move (turn d t)
+step :: (Coord, Direction) -> Turn -> (Coord, Direction)
+step ((y, x), d) t = move (turn d t)
   where
     turn N (L _) = W
     turn W (L _) = S
@@ -40,7 +39,7 @@ step (y, x, d) t = move (turn d t)
     turn S (R _) = W
     turn W (R _) = N
 
-    move d' = let (dy, dx) = delta d' in (y + dy * n, x + dx * n, d')
+    move d' = let (dy, dx) = delta d' in ((y + dy * n, x + dx * n), d')
 
     n = case t of L k -> k; R k -> k
 
@@ -50,21 +49,18 @@ delta E = (0 , 1)
 delta S = (1 , 0)
 delta W = (0 , -1)
 
-toCoord :: CoordD -> Coord
-toCoord (y, x, _) = (y, x)
-
 {------------------------------{ 2nd part }------------------------------}
 
 day01b :: String -> String
 day01b = show . part2 . parseLine
 
 part2 :: [Turn] -> Int
-part2 = manhattan . findFirstDuped . expandPath . scanl step (0, 0, N)
+part2 = manhattan . findFirstDuped . expandPath . scanl step ((0, 0), N)
 
-expandPath :: [CoordD] -> [Coord]
+expandPath :: [(Coord, Direction)] -> [Coord]
 expandPath xs = concat $ zipWith expand xs (tail xs)
   where
-    expand (y1,x1,_) (y2,x2,_)
+    expand ((y1,x1),_) ((y2,x2),_)
       | x1 == x2 = [(y,x1) | y <- range y1 y2]
       | y1 == y2 = [(y1,x) | x <- range x1 x2]
       | otherwise = []
