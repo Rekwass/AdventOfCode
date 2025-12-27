ea05a4451bd3506ce73c5fdce826042684b8f361
Rekwass <rekwasss@gmail.com>
2025-12-27 11:44:53 +0100

    feat: day10

--- 2016/haskell/src/Days/Day10.hs
+++ 2016/haskell/src/Days/Day10.hs
@@ -1,16 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -Wno-x-partial #-}
+{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 
 module Days.Day10 (day10a, day10b) where
 
 {------------------------------{ 1st part }------------------------------}
 
-import Data.List (foldl', sort, find)
+import Data.List (foldl', sort)
 import qualified Data.Text as T
 import qualified Data.Map as M
 import Text.Parsec
 import Text.Parsec.Text (Parser)
-import Data.Maybe (fromJust)
 
 -- 0, 1 or 2 values
 type Values = [Int]
@@ -18,21 +19,22 @@ type Robots = M.Map Id Values
 
 data Id = Robot Int | Output Int deriving (Eq, Ord, Show)
 
---                  IdB IdL IdH
-type Instruction = (Id, Id, Id)
+--                  IdL IdH
+type Instruction = (Id, Id)
+type Instructions = M.Map Id Instruction
 
-data Line' = Val (Int, Id) | Trade Instruction
+data Line' = Val (Int, Id) | Trade (Id, Id, Id)
 
 day10a :: String -> String
 day10a = show . part1 . parseLines . T.lines . T.pack
 
-parseLines :: [T.Text] -> (Robots, [Instruction])
-parseLines = foldl' step (M.empty, []) . map parseLine
+parseLines :: [T.Text] -> (Robots, Instructions)
+parseLines = foldl' step (M.empty, M.empty) . map parseLine
   where
     parseLine = either (error . show) id . parse lineParser ""
     step (rs, is) = \case
-      Val (val, botId) -> (M.insertWith (<>) botId [val] rs, is)
-      Trade is'        -> (rs, is':is)
+      Val   (val, botId)    -> (M.insertWith (<>) botId [val] rs, is)
+      Trade (botId, bl, bh) -> (rs, M.insert botId (bl, bh) is)
 
 lineParser :: Parser Line'
 lineParser = try (Val <$> valueParser) <|> Trade <$> tradeParser
@@ -42,7 +44,7 @@ valueParser = (,)
   <$> (string "value " *> number)
   <*> (string " goes to bot " *> (Robot <$> number))
 
-tradeParser :: Parser Instruction
+tradeParser :: Parser (Id, Id, Id)
 tradeParser = (,,)
   <$> (string "bot " *> (Robot <$> number))
   <*> (string " gives low to " *> idParser)
@@ -56,50 +58,44 @@ idParser =
 number :: Parser Int
 number = read <$> many1 digit
 
-part1 :: (Robots, [Instruction]) -> Int
+part1 :: (Robots, Instructions) -> Int
 part1 = getHim . fst . head . dropWhile notCmp61and17 . iterate step
   where
-    step (rs, is) = (foldr (updateRobot is) rs twoValRobots, is)
-      where
-        twoValRobots = M.keys $ M.filter ((==2) . length) rs
+    step (rs, is) = (foldr (updateRobot is) rs (twoValRobots rs), is)
 
-updateRobot :: [Instruction] -> Id -> Robots -> Robots
-updateRobot is botId rs = M.union rs' rs 
+twoValRobots :: Robots -> [Id]
+twoValRobots = M.keys . M.filter ((==2) . length)
+
+updateRobot :: Instructions -> Id -> Robots -> Robots
+updateRobot is botId rs =
+    M.insert botId []
+  . M.insertWith (<>) bl [minimum val]
+  . M.insertWith (<>) bh [maximum val]
+  $ rs
   where
-    (_, bl, bh) = fromJust $ find (\(iid, _, _) -> iid == botId) is
-    bg  = M.findWithDefault [] botId rs
-    bl' = minimum bg : M.findWithDefault [] bl rs
-    bh' = maximum bg : M.findWithDefault [] bh rs
-    rs' = M.fromList [(botId, []), (bl, bl'), (bh, bh')]
+    (bl, bh) = is M.! botId
+    val      = M.findWithDefault [] botId rs
 
 getHim :: Robots -> Int
-getHim rs = botId
-  where
-    Robot botId = head $ M.keys $ M.filterWithKey isGood rs
+getHim = (\(Robot botId) -> botId) . head . M.keys . M.filterWithKey isGood
 
-notCmp61and17 :: (Robots, [Instruction]) -> Bool
+notCmp61and17 :: (Robots, Instructions) -> Bool
 notCmp61and17 (rs, _) = M.null $ M.filterWithKey isGood rs
 
 isGood :: Id -> Values -> Bool
-isGood (Output _) _ = False
-isGood (Robot _) ns = sort ns == [17, 61]
+isGood (Output _) _  = False
+isGood (Robot _)  ns = sort ns == [17, 61]
 
 {------------------------------{ 2nd part }------------------------------}
 
 day10b :: String -> String
 day10b = show . part2 . parseLines . T.lines . T.pack
 
-part2 :: (Robots, [Instruction]) -> Int
+part2 :: (Robots, Instructions) -> Int
 part2 = get012 . fst . head . dropWhile robotHas2val . iterate step
   where
-    robotHas2val = not . M.null . M.filter ((==2) . length) . fst
-    step (rs, is) = (foldr (updateRobot is) rs twoValRobots, is)
-      where
-        twoValRobots = M.keys $ M.filter ((==2) . length) rs
+    robotHas2val = any ((==2) . length) . fst
+    step (rs, is) = (foldr (updateRobot is) rs (twoValRobots rs), is)
 
 get012 :: Robots -> Int
-get012 rs = product $ zero <> one <> two
-  where
-    zero = rs M.! Output 0
-    one = rs M.! Output 1
-    two = rs M.! Output 2
+get012 rs = product $ concat [rs M.! Output i | i <- [0..2]]
